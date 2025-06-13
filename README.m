82c9c8eef99281aac2269be246b9dd6ae595f8de
MintTea <88380103+MintOcha@users.noreply.github.com>
2025-06-13 15:12:28 +0800

    Delete README.m

--- README.m
+++
@@ -1,4 +0,0 @@
-# GrifflesCTF 25 challs and writeups
-## Contains: Funny gojo chall (i-alone-am-the-honored-one), basic-buffer-overflow and the 1k point griffles RETurns challs
-File format: Each challenge has their own folder, under there you can find writeup.md as well as my solve script :3
-Enjoy~
